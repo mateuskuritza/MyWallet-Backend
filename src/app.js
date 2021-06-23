992d49b9bd7f9d5eb40fe7b53752cb0a6c228103
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-06-23 01:02:28 -0300

    backend finalizado

--- src/app.js
+++ src/app.js
@@ -1,2 +1,91 @@
 import app from "./server.js";
 import connection from "./database/database.js";
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+import dayjs from "dayjs";
+
+app.post("/register", async (req, res) => {
+	const { name, email, password } = req.body;
+	if (!name || !email || !password) {
+		return res.sendStatus(400);
+	}
+	const existingEmail = await connection.query(`SELECT * FROM users WHERE email = $1`, [email]);
+	if (existingEmail.rows.length > 0) {
+		return res.sendStatus(409);
+	}
+
+	const hashPassword = bcrypt.hashSync(password, 12);
+	await connection.query(`INSERT INTO users (name, email, password) values ($1,$2,$3)`, [name, email, hashPassword]);
+	res.sendStatus(201);
+});
+
+app.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.sendStatus(400);
+	}
+	const existingUser = await connection.query(`SELECT * FROM users WHERE email = $1`, [email]);
+	const user = existingUser.rows[0];
+
+	if (user && bcrypt.compareSync(password, user.password)) {
+		const token = uuidv4();
+		await connection.query(`INSERT INTO "userToken" ("userId", token) values ($1,$2)`, [user.id, token]);
+		const resp = {
+			name: user.name,
+			id: user.id,
+			token,
+		};
+		return res.send(resp);
+	}
+
+	res.sendStatus(401);
+});
+
+app.get("/registers", async (req, res) => {
+	const authorization = req.headers["authorization"];
+	if (!authorization) {
+		return res.sendStatus(400);
+	}
+	const user = await authUser(authorization);
+	if (!user) {
+		return res.sendStatus(401);
+	}
+	const registers = await connection.query(`SELECT * FROM register WHERE "userId" = $1`, [user.id]);
+	res.send(registers.rows);
+});
+
+app.post("/registers/:type", async (req, res) => {
+	const authorization = req.headers["authorization"];
+	const { description, value } = req.body;
+	const { type } = req.params;
+	if (!description || !value || !["revenue", "expense"].includes(type) || !authorization) {
+		return res.sendStatus(400);
+	}
+	const user = await authUser(authorization);
+	if (!user) {
+		return res.sendStatus(401);
+	}
+	const today = new Date();
+	await connection.query(`INSERT INTO register (date, description, value, type, "userId") values ($1,$2,$3,$4,$5)`, [
+		today,
+		description,
+		value,
+		type,
+		user.id,
+	]);
+	res.sendStatus(201);
+});
+
+async function authUser(authorization) {
+	const token = authorization.replace("Bearer ", "");
+	const userDb = await connection.query(
+		`
+    SELECT * FROM "userToken"
+    JOIN users
+    ON "userToken"."userId" = users.id
+    WHERE "userToken".token = $1`,
+		[token]
+	);
+	const user = userDb.rows[0];
+	return user;
+}
