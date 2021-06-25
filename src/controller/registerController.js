100b65262d5b03b793c80d1d994e679a97542fa4
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-06-25 17:51:08 -0300

    try catch add

--- src/controller/registerController.js
+++ src/controller/registerController.js
@@ -3,47 +3,63 @@ import authUser from "./authUser.js";
 
 const Register = {
 	async registers(req, res) {
-		const authorization = req.headers["authorization"];
-		if (!authorization) return res.sendStatus(400);
-		const user = await authUser(authorization);
-		if (!user) return res.sendStatus(401);
-		const registers = (await connection.query(`SELECT * FROM register WHERE "userId" = $1`, [user.id])).rows;
+		try {
+			const authorization = req.headers["authorization"];
+			if (!authorization) return res.sendStatus(400);
+			const user = await authUser(authorization);
+			if (!user) return res.sendStatus(401);
+			const registers = (await connection.query(`SELECT * FROM register WHERE "userId" = $1`, [user.id])).rows;
 
-		const revenue = (
-			await connection.query(
-				`SELECT SUM(value) AS total FROM register WHERE register.type='revenue' AND register."userId"=$1`,
-				[user.id]
-			)
-		).rows[0];
-		const expense = (
-			await connection.query(
-				`SELECT SUM(value) AS total FROM register WHERE register.type='expense' AND register."userId"=$1`,
-				[user.id]
-			)
-		).rows[0];
-		const balance = (revenue.total - expense.total).toFixed(2);
-		const resp = {
-			registers,
-			balance: parseFloat(balance),
-		};
-		res.send(resp);
+			const revenue = (
+				await connection.query(
+					`SELECT SUM(value) AS total FROM register WHERE register.type='revenue' AND register."userId"=$1`,
+					[user.id]
+				)
+			).rows[0];
+			const expense = (
+				await connection.query(
+					`SELECT SUM(value) AS total FROM register WHERE register.type='expense' AND register."userId"=$1`,
+					[user.id]
+				)
+			).rows[0];
+			const balance = (revenue.total - expense.total).toFixed(2);
+			const resp = {
+				registers,
+				balance: parseFloat(balance),
+			};
+			res.send(resp);
+		} catch {
+			res.sendStatus(500);
+		}
 	},
 	async newRegister(req, res) {
-		const authorization = req.headers["authorization"];
-		const { description, value } = req.body;
-		const { type } = req.params;
-		if (!description || !value || !isNaN(value) || value <= 0 || !["revenue", "expense"].includes(type) || !authorization) return res.sendStatus(400);
-		const user = await authUser(authorization);
-		if (!user) return res.sendStatus(401);
-		const today = new Date();
-		await connection.query(`INSERT INTO register (date, description, value, type, "userId") values ($1,$2,$3,$4,$5)`, [
-			today,
-			description,
-			value,
-			type,
-			user.id,
-		]);
-		res.sendStatus(201);
+		try {
+			const authorization = req.headers["authorization"];
+			const { description, value } = req.body;
+			const { type } = req.params;
+			if (
+				!description ||
+				!value ||
+				isNaN(value) ||
+				value <= 0 ||
+				!["revenue", "expense"].includes(type) ||
+				!authorization
+			)
+				return res.sendStatus(400);
+			const user = await authUser(authorization);
+			if (!user) return res.sendStatus(401);
+			const today = new Date();
+			await connection.query(`INSERT INTO register (date, description, value, type, "userId") values ($1,$2,$3,$4,$5)`, [
+				today,
+				description,
+				value,
+				type,
+				user.id,
+			]);
+			res.sendStatus(201);
+		} catch {
+			res.sendStatus(500);
+		}
 	},
 };
 
--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -5,39 +5,51 @@ import authUser from "./authUser.js";
 
 const User = {
 	async register(req, res) {
-		const { name, email, password } = req.body;
-		if (!name || !email || !password) return res.sendStatus(400);
-		const existingEmail = (await connection.query(`SELECT * FROM users WHERE email = $1`, [email])).rows;
-		if (existingEmail.length > 0) return res.sendStatus(409);
-		const hashPassword = bcrypt.hashSync(password, 12);
-		await connection.query(`INSERT INTO users (name, email, password) values ($1,$2,$3)`, [name, email, hashPassword]);
-		res.sendStatus(201);
+		try {
+			const { name, email, password } = req.body;
+			if (!name || !email || !password) return res.sendStatus(400);
+			const existingEmail = (await connection.query(`SELECT * FROM users WHERE email = $1`, [email])).rows;
+			if (existingEmail.length > 0) return res.sendStatus(409);
+			const hashPassword = bcrypt.hashSync(password, 12);
+			await connection.query(`INSERT INTO users (name, email, password) values ($1,$2,$3)`, [name, email, hashPassword]);
+			res.sendStatus(201);
+		} catch {
+			res.sendStatus(500);
+		}
 	},
 	async login(req, res) {
-		const { email, password } = req.body;
-		if (!email || !password) return res.sendStatus(400);
-		const user = (await connection.query(`SELECT * FROM users WHERE email = $1`, [email])).rows[0];
+		try {
+			const { email, password } = req.body;
+			if (!email || !password) return res.sendStatus(400);
+			const user = (await connection.query(`SELECT * FROM users WHERE email = $1`, [email])).rows[0];
 
-		if (user && bcrypt.compareSync(password, user.password)) {
-			const token = uuidv4();
-			await connection.query(`DELETE FROM "userToken" WHERE "userId" = $1`, [user.id]);
-			await connection.query(`INSERT INTO "userToken" ("userId", token) values ($1,$2)`, [user.id, token]);
-			const resp = {
-				name: user.name,
-				token,
-			};
-			return res.send(resp);
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = uuidv4();
+				await connection.query(`DELETE FROM "userToken" WHERE "userId" = $1`, [user.id]);
+				await connection.query(`INSERT INTO "userToken" ("userId", token) values ($1,$2)`, [user.id, token]);
+				const resp = {
+					name: user.name,
+					token,
+				};
+				return res.send(resp);
+			}
+
+			res.sendStatus(401);
+		} catch {
+			res.sendStatus(500);
 		}
-
-		res.sendStatus(401);
 	},
 	async logout(req, res) {
-		const authorization = req.headers["authorization"];
-		if (!authorization) return res.sendStatus(400);
-		const user = await authUser(authorization);
-		if (!user) return res.sendStatus(401);
-		await connection.query(`DELETE FROM "userToken" WHERE token = $1`, [user.token]);
-		res.sendStatus(200);
+		try {
+			const authorization = req.headers["authorization"];
+			if (!authorization) return res.sendStatus(400);
+			const user = await authUser(authorization);
+			if (!user) return res.sendStatus(401);
+			await connection.query(`DELETE FROM "userToken" WHERE token = $1`, [user.token]);
+			res.sendStatus(200);
+		} catch {
+			res.sendStatus(500);
+		}
 	},
 };
 
