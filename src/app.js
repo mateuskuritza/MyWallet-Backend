9f3265fc8021b2b14d8d426a39b68bb7abf12af3
ronaldjunor2 <ronaldjunior2@hotmail.com>
2021-06-30 18:30:11 -0300

    preparando review

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import routes from "./routes/routes.js";
-import express from "express";
-import cors from "cors";
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", routes);
-
-export default app;
--- src/tests/loginUser.js
+++
@@ -1,9 +0,0 @@
-import supertest from "supertest";
-import app from "../app";
-
-async function loginUser(email, password) {
-	const result = await supertest(app).post("/user/login").send({ email, password });
-	return result.body.token;
-}
-
-export default loginUser;
