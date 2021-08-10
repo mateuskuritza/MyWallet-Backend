a5c8e0535c49c9924c762f6040f98739d1fec290
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-08-10 16:01:49 -0300

    change variable name resp

--- src/controller/registerController.js
+++ src/controller/registerController.js
@@ -3,58 +3,58 @@ import authUser from "./authUser.js";
 import { newRegisterSchema } from "../schemasJoi/schemas.js";
 
 const Register = {
-	async registers(req, res) {
-		try {
-			const authorization = req.headers["authorization"];
-			if (!authorization) return res.sendStatus(400);
-			const user = await authUser(authorization);
-			if (!user) return res.sendStatus(401);
-			const registers = (await connection.query(`SELECT * FROM register WHERE "userId" = $1`, [user.id])).rows;
+    async registers(req, res) {
+        try {
+            const authorization = req.headers["authorization"];
+            if (!authorization) return res.sendStatus(400);
+            const user = await authUser(authorization);
+            if (!user) return res.sendStatus(401);
+            const registers = (await connection.query(`SELECT * FROM register WHERE "userId" = $1`, [user.id])).rows;
 
-			const revenue = (
-				await connection.query(
-					`SELECT SUM(value) AS total FROM register WHERE register.type='revenue' AND register."userId"=$1`,
-					[user.id]
-				)
-			).rows[0];
-			const expense = (
-				await connection.query(
-					`SELECT SUM(value) AS total FROM register WHERE register.type='expense' AND register."userId"=$1`,
-					[user.id]
-				)
-			).rows[0];
-			const balance = (revenue.total - expense.total).toFixed(2);
-			const resp = {
-				registers,
-				balance: parseFloat(balance),
-			};
-			res.send(resp);
-		} catch {
-			res.sendStatus(500);
-		}
-	},
-	async newRegister(req, res) {
-		try {
-			const authorization = req.headers["authorization"];
-			const { description, value } = req.body;
-			const { type } = req.params;
-			if (newRegisterSchema.validate(req.body).error || !["revenue", "expense"].includes(type) || !authorization)
-				return res.sendStatus(400);
-			const user = await authUser(authorization);
-			if (!user) return res.sendStatus(401);
-			const today = new Date();
-			await connection.query(`INSERT INTO register (date, description, value, type, "userId") values ($1,$2,$3,$4,$5)`, [
-				today,
-				description,
-				value,
-				type,
-				user.id,
-			]);
-			res.sendStatus(201);
-		} catch {
-			res.sendStatus(500);
-		}
-	},
+            const revenue = (
+                await connection.query(
+                    `SELECT SUM(value) AS total FROM register WHERE register.type='revenue' AND register."userId"=$1`,
+                    [user.id]
+                )
+            ).rows[0];
+            const expense = (
+                await connection.query(
+                    `SELECT SUM(value) AS total FROM register WHERE register.type='expense' AND register."userId"=$1`,
+                    [user.id]
+                )
+            ).rows[0];
+            const balance = (revenue.total - expense.total).toFixed(2);
+            const registersWithBalance = {
+                registers,
+                balance: parseFloat(balance),
+            };
+            res.send(registersWithBalance);
+        } catch {
+            res.sendStatus(500);
+        }
+    },
+    async newRegister(req, res) {
+        try {
+            const authorization = req.headers["authorization"];
+            const { description, value } = req.body;
+            const { type } = req.params;
+            if (newRegisterSchema.validate(req.body).error || !["revenue", "expense"].includes(type) || !authorization)
+                return res.sendStatus(400);
+            const user = await authUser(authorization);
+            if (!user) return res.sendStatus(401);
+            const today = new Date();
+            await connection.query(`INSERT INTO register (date, description, value, type, "userId") values ($1,$2,$3,$4,$5)`, [
+                today,
+                description,
+                value,
+                type,
+                user.id,
+            ]);
+            res.sendStatus(201);
+        } catch {
+            res.sendStatus(500);
+        }
+    },
 };
 
 export default Register;
